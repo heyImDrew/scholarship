1dc54422c9845ef82ec48de9ddad96d1e0f3edfc
heyImDrew <drew.forall@gmail.com>
2020-12-05 07:45:38 +0300

    [ADD] exam info

--- Server/src/Connection/Action.java
+++ Server/src/Connection/Action.java
@@ -5,6 +5,7 @@ import DBConnection.ConnectionClass;
 import javax.xml.transform.Result;
 import java.io.IOException;
 import java.sql.*;
+import java.util.ArrayList;
 
 public class Action {
     public void execute(Handler handler, String action) throws IOException, ClassNotFoundException, SQLException {
@@ -94,7 +95,37 @@ public class Action {
             }
 
             case "loadExamInfo": {
-                System.out.println("ExamInfo");
+                Integer student_id = (Integer) handler.read();
+
+                ConnectionClass connectionClass = new ConnectionClass();
+                Connection connection = connectionClass.getConnection();
+                String query = "SELECT session FROM scholarship.student WHERE idStudent = ?";
+                PreparedStatement preparedStmt = connection.prepareStatement(query);
+                preparedStmt.setInt(1, student_id);
+                ResultSet res = preparedStmt.executeQuery();
+                res.next();
+
+                int session_num = res.getInt("session");
+
+                String query1 = "SELECT * FROM scholarship.exam WHERE Session = ?";
+                PreparedStatement preparedStmt1 = connection.prepareStatement(query1);
+                preparedStmt1.setInt(1, session_num);
+                ResultSet res1 = preparedStmt1.executeQuery();
+                while (res1.next()) {
+                    ArrayList r = new ArrayList();
+                    r.add(res1.getString("subject"));
+                    r.add(res1.getString("date"));
+                    r.add(res1.getString("mark"));
+                    System.out.println(r.get(0));
+                    System.out.println(r.get(1));
+                    System.out.println(r.get(2));
+                    handler.write(r);
+                    r.clear();
+                }
+                ArrayList r = new ArrayList();
+                r.clear();
+                r.add("stop");
+                handler.write(r);
                 break;
             }
         }
